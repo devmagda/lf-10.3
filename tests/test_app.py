01b00687abf14865f862d115eae8327428f10ec6
devmagda <113769198+devmagda@users.noreply.github.com>
2024-11-27 18:43:30 +0100

    Added more tests
    Added Integration tests to test the full app

---
+++ tests/test_app.py
@@ -0,0 +1,18 @@
+import pytest
+
+from app import create_app
+
+@pytest.fixture
+def app():
+    return create_app()
+
+def test_app():
+    app = create_app()
+    assert app is not None
+
+def test_blueprints_registered(app):
+    # Check if the blueprints are registered
+    blueprints = [bp.name for bp in app.blueprints.values()]
+    expected_blueprints = ['auth', 'roles', 'global', 'views', 'events']  # List of blueprint names
+    for blueprint in expected_blueprints:
+        assert blueprint in blueprints
